608ff2ae16283958e8a13313ec0703a61260b6c6
Ludmil Simeonov <lsimeonov@despark.com>
2016-04-26 11:55:37 +0300

    Only extend the base validator without creating new custom instance

--- src/Mpociot/VatCalculator/Validators/VatCalculatorValidatorExtension.php
+++ src/Mpociot/VatCalculator/Validators/VatCalculatorValidatorExtension.php
@@ -6,33 +6,25 @@ use Illuminate\Validation\Validator;
 use Mpociot\VatCalculator\Exceptions\VATCheckUnavailableException;
 use Mpociot\VatCalculator\Facades\VatCalculator;
 
-class VatCalculatorValidatorExtension extends Validator
+class VatCalculatorValidatorExtension
 {
-    /**
-     * Creates a new instance of ValidatorExtension.
-     */
-    public function __construct($translator, $data, $rules, $messages, array $customAttributes = [])
-    {
-        // Set custom validation error messages
-        if (!isset($messages['vat_number'])) {
-            $messages['vat_number'] = $translator->get(
-                'vatnumber-validator::validation.vat_number'
-            );
-        }
-        parent::__construct($translator, $data, $rules, $messages, $customAttributes);
-    }
 
     /**
      * Usage: vat_number.
      *
      * @param string $attribute
-     * @param mixed  $value
-     * @param array  $parameters
+     * @param mixed $value
+     * @param array $parameters
+     * @param $validator
      *
      * @return bool
      */
-    public function validateVatNumber($attribute, $value, $parameters)
+    public function validateVatNumber($attribute, $value, $parameters, $validator)
     {
+        $validator->setCustomMessages([
+            'vat_number' => $validator->getTranslator()->get('vatnumber-validator::validation.vat_number'),
+        ]);
+
         try {
             return VatCalculator::isValidVATNumber($value);
         } catch (VATCheckUnavailableException $e) {
