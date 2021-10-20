84b69a28b8ffb2448fd22f8d971ddb47771af1d8
Dries Vints <dries@vints.io>
2021-10-20 19:20:18 +0200

    wip

--- src/VatCalculatorServiceProvider.php
+++ src/VatCalculatorServiceProvider.php
@@ -2,87 +2,38 @@
 
 namespace Mpociot\VatCalculator;
 
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class VatCalculatorServiceProvider extends ServiceProvider
+class VatCalculatorServiceProvider extends ServiceProvider implements DeferrableProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
-
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
+    public function register(): void
     {
-        $this->publishConfig();
+        $this->mergeConfig();
+        $this->registerVatCalculator();
     }
 
-    /**
-     * Publish configuration.
-     */
-    protected function publishConfig()
+    public function boot(): void
     {
         $this->publishes([
             __DIR__.'/../config/vat_calculator.php' => config_path('vat_calculator.php'),
         ]);
     }
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
+    protected function mergeConfig(): void
     {
-        $this->mergeConfig();
-        $this->registerVatCalculator();
-        $this->registerFacade();
+        $this->mergeConfigFrom(__DIR__.'/../config/vat_calculator.php', 'vat_calculator');
     }
 
-    /**
-     * Register the application bindings.
-     *
-     * @return void
-     */
-    protected function registerVatCalculator()
+    protected function registerVatCalculator(): void
     {
-        $this->app->bind('vatcalculator', \Mpociot\VatCalculator\VatCalculator::class);
+        $this->app->bind(VatCalculator::class, function ($app) {
+            $config = $app->make(Repository::class);
 
-        $this->app->bind(\Mpociot\VatCalculator\VatCalculator::class, function ($app) {
-            $config = $app->make('Illuminate\Contracts\Config\Repository');
-
-            return new \Mpociot\VatCalculator\VatCalculator($config);
+            return new VatCalculator($config);
         });
-    }
 
-    /**
-     * Register the vault facade without the user having to add it to the app.php file.
-     *
-     * @return void
-     */
-    public function registerFacade()
-    {
-        $this->app->booting(function () {
-            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-            $loader->alias('VatCalculator', 'Mpociot\VatCalculator\Facades\VatCalculator');
-        });
-    }
-
-    /**
-     * Merges user's and teamwork's configs.
-     *
-     * @return void
-     */
-    protected function mergeConfig()
-    {
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/vat_calculator.php', 'vat_calculator'
-        );
+        $this->app->bind('vatcalculator', VatCalculator::class);
     }
 }
