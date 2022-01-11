cf71081239f9410c8b480313f9f57f653e5e2839
Michal Špaček <mail@michalspacek.cz>
2022-01-11 09:02:20 +0100

    Campione d'Italia, IT became part of the EU customs territory on 1 Jan 2020 (#138)
    Campione d’Italia formally became part of the EU customs territory on 1 January 2020. VAT will apply, but the tax rate will remain that of Switzerland. Also, the Swiss postal code ceased to be valid.

--- src/VatCalculator.php
+++ src/VatCalculator.php
@@ -105,7 +105,7 @@ class VatCalculator
         'IT' => [ // Italy
             'rate' => 0.22,
             'exceptions' => [
-                'Campione d\'Italia' => 0,
+                'Campione d\'Italia' => 0.077,
                 'Livigno' => 0,
             ],
         ],
@@ -193,11 +193,6 @@ class VatCalculator
                 'postalCode' => '/^8238$/',
                 'code' => 'DE',
                 'name' => 'Büsingen am Hochrhein',
-            ],
-            [
-                'postalCode' => '/^6911$/',
-                'code' => 'IT',
-                'name' => "Campione d'Italia",
             ],
             // The Italian city of Domodossola has a Swiss post office also
             [
